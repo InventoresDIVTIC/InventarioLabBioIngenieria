dcc5f267bba2871b45ee81162485a1c700bad508
Tonatiou <lolstonatiou@gmail.com>
2023-10-26 19:06:23 -0600

    edit-upload

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://bioingenieria.inventores.org/resources/css/edit-perfil.css">
+  <link rel="stylesheet" href="https://bioingenieria.inventores.org/css/edit-perfil.css">
   <link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <title>MI PERFIL</title>
 </head>
