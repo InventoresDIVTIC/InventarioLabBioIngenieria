098653b5b4b76f83b411091ee9347523ecbed8de
Arturo Nila <arturonilaolid@gmail.com>
2024-02-01 18:35:50 -0600

    Texto de paginas de error

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Error del Servidor'))
 @section('code', '500')
-@section('message', __('Error del Servidor, Correo ya existente. Favor de intentar de nuevo.'))
+@section('message', __('Error del Servidor'))
