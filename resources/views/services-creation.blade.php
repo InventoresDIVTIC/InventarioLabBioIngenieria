629d5e11fecf4b5966ba3ceacdf022c84513a7c9
sergiog-dev <sergiog.dev@gmail.com>
2024-02-20 19:26:33 -0600

    fix js url

--- resources/views/services-creation.blade.php
+++ resources/views/services-creation.blade.php
@@ -129,7 +129,7 @@
                     });
                 </script>
 
-                <script src="http://127.0.0.1:8000/js/services-creation.js"></script>
+                <script src="https://bioingenieria.inventores.org/js/services-creation.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
                 <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
                 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
