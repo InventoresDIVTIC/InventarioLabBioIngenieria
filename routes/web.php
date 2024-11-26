003dbb59b30a6b2fef5b2411841e3cd7f187d2fb
sergiog-dev <sergiog.dev@gmail.com>
2024-11-25 19:59:02 -0600

    dashboard veridied

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::get('/users-edit', function () {
 })->middleware(['auth', 'verified'])->name('users-edit');
 
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
